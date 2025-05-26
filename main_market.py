6e06e5ad03290060a9872fff88ead81a9938cde8
Geovane Ferreira <geovane.paixao@ba.estudante.senai.br>
2025-05-25 22:23:01 -0300

    Atualização da aplicação Mercado 20250525
    Atualização da aplicação Mercado 20250525

--- main_market.py
+++ main_market.py
@@ -1,13 +1,15 @@
 # os "import" serve para trazer dados/funções de cada módulo/arquivo criado.
-import produto
+import produto_market
+import vendas_market
+import cupom_market
 
 # função "opmenu" serve para chamar o menu completo na tela de prompt.
 
 
 def opmenu():
-    print("\n|-----------    ⛽ MERCADO GGK - MENU DE ATIVIDADES       -----------------|")
+    print("\n|-----------    🛒 MERCADO GGK - MENU DE ATIVIDADES       -----------------|")
     print("| 1. CADASTRO DE PRODUTO.     | 4. PESQUISAR POR SESSÃO.                   |")
-    print("| 2. LISTAR PRODUTOS.         | 5.                                         |")
+    print("| 2. LISTAR PRODUTOS.         | 5. CAIXA VENDAS.                           |")
     print("| 3. PESQUISAR POR CÓDIGO.    | 0. ENCERRAR PROGRAMA .                     |\n")
 
 while True:
@@ -15,13 +17,20 @@ while True:
     oper = input("Selecione uma Opção. ")
     match oper:
         case "1":
-            produto.cadastro_produto()
+            produto_market.cadastro_produto()
         case "2":
-            produto.listarProduto()
+            produto_market.listarProduto()
         case "3":
-            produto.pesquisaritem()  
+            produto_market.pesquisaritem()  
         case "4":
-            produto.pesquisarsessao()
+            produto_market.pesquisarsessao()
+        case "5":
+            #vendas_market.vendasCaixa()
+            codigo, valor_total, quant_item = vendas_market.vendasCaixa()
+            
+           # cupom_market.cupom(codigo, valor_total, quant_item,)
+            cupom_market.cupom(codigo, valor_total, quant_item)
+
         case "_":
             print("Opção Inválida.")
         case "0":
